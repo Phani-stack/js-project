c175d450b9de4ae049a10ef0713fdea149b3ba72
Phani-stack <kdlphani916@gmial.com>
2025-12-25 08:53:32 +0530

    Change made in project

--- server/index.js
+++ server/index.js
@@ -1,20 +1,52 @@
-import express from "express";
+import express, { response } from "express";
 import "dotenv/config";
+import { rateLimit } from 'express-rate-limit';
+import { query, validationResult } from 'express-validator';
 
 const app = express();
+
+app.use(express.json());
+
+const limiter = rateLimit({
+    limit: 3,
+    windowMs: 60 * 1000,
+    message: "Too many requests please try after some time."
+});
+
 
 const port = process.env.PORT;
 
 app.get("/", (request, response) => {
     try {
-        response.status(200).json({ 
-            "message" : "Connection established"
+        response.status(200).json({
+            "message": "Connection established"
         });
     } catch (error) {
-        response.status(404).json({
-            "message" : error
+        response.status(500).json({
+            "message": error
         });
     }
 });
 
-app.listen(port, () => console.log("Application Running on http://localhost:" + port));
+app.get("/rate-limit-testing", limiter, (request, response) => {
+    try {
+        response.status(200).json({ message: "Working" })
+    } catch (error) {
+        response.status(400).json({ error: error });
+    }
+});
+
+
+app.get("/express-validator", query('person').notEmpty(), (request, response) => {
+    const result = validationResult(request);
+    if (result.isEmpty()) {
+        return response.send(`Hello, ${request.query.person}!`);
+    }
+
+    response.send({ errors: result.array() });
+});
+
+
+app.listen(port, () => {
+    console.log("Application Running on http://localhost:" + port)
+});
